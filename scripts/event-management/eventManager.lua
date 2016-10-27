eff882bf21b1afa60cd9d4964ba951ee9577b096
benjamin <updegraft.ben@gmail.com>
2016-10-26 20:15:45 -0400

    Implemented event manager

--- scripts/event-management/eventManager.lua
+++ scripts/event-management/eventManager.lua
@@ -2,21 +2,34 @@ eventQueue = require("../utils.queue")
 handlers = {}
 
 function register(handler)
-    eventType = handler.eventType
-    if handlers[eventType] == nil then
-        handlers[eventType] = require("../utils.queue")
-    end
-    handlers[eventType].pushLeft(handler)
+  eventType = handler.eventType
+  if handlers[eventType] == nil then
+      handlers[eventType] = require("../utils.queue")
+  end
+  handlers[eventType].pushLeft(handler)
 end
 
-function notify(event)
-
+function notify(handler, event)
+  handler.notify(event)
 end
 
 function raise(event)
+  eventQueue.pushRight(event)
 end
 
 function handleEvents(timestamp)
+  idx = eventQueue.first
+  current = eventQueue[idx]
+  while eventQueue[idx] ~= nil then
+    event = eventQueue[idx]
+    notify(handlers[event.type], event)
+    idx += 1
+  end
 end
 
 local module = {}
+module.register = register
+module.notify = notify
+module.raise = raise
+module.handleEvents = handleEvents
+return module
